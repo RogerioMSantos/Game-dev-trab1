ddce11dc8230c1b717418c42312e9b3c2eae65bd
João Milli <joao.biancardi@edu.ufes.br>
2024-08-20 23:59:15 -0300

    Adicionar sprites e movimentos do player

--- Assets/Scripts/Game/Player/playerMovement.cs
+++ Assets/Scripts/Game/Player/playerMovement.cs
@@ -7,22 +7,46 @@ public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private Vector2 _movementInput;
+    private Vector2 _smoothMovementInput;
+    private Vector2 _movementInputSmoothVelocity;
 
     [SerializeField]
     private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void SetPlayerVelocity()
+    {
+        _smoothMovementInput = Vector2.SmoothDamp(_smoothMovementInput, _movementInput, ref _movementInputSmoothVelocity, 0.1f);
+
+        _rb.velocity = _smoothMovementInput * _speed;
+    }
+
     private void FixedUpdate()
     {
-        _rb.velocity = _movementInput * _speed;
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
+    }
+
+    private void RotateInDirectionOfInput()
+    {
+        if (_movementInput != Vector2.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(transform.forward, _smoothMovementInput);
+            Quaternion finalRotation = Quaternion.RotateTowards(transform.rotation, newRotation, _rotationSpeed * Time.deltaTime);
+
+            _rb.MoveRotation(finalRotation);
+        }
     }
 
     private void OnMove(InputValue inputValue)
     {
-       _movementInput = inputValue.Get<Vector2>();
+        _movementInput = inputValue.Get<Vector2>();
     }
 }
