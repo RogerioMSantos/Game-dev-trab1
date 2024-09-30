93a141777e6a653c336bd15db9fd86415a0bc676
João Milli <joao.biancardi@edu.ufes.br>
2024-09-30 15:47:20 -0300

    Adicionar inimigos

--- Assets/Scripts/Game/Objects/Key.cs
+++ Assets/Scripts/Game/Objects/Key.cs
@@ -7,8 +7,12 @@ public class Key : MonoBehaviour
     [SerializeField] private KeyType keyType;
     public enum KeyType
     {
-        Ruby,
-        Gold,
+        Ruby1,
+        Ruby2,
+        Ruby3,
+        Gold1,
+        Gold2,
+        Gem,
     }
 
     public KeyType GetKeyType()
